ab5f7ef3d375d3d65835a938350a741202d47b37
20040608 <issakvell@gmail.com>
2022-09-15 22:38:48 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,331 @@
+//------------------------------------------------For
+
+//-------------------  1-masala
+
+// let a = 8
+// let b = 9 
+
+// for (i = 0; i <= b; i++ ){
+//     console.log(a);
+// }
+    
+
+//-------------------  2-masala
+
+// let a = 11 
+// let b = 9
+
+// for (let i = b; i < a; i++) {
+//     console.log(i);
+// }
+
+  
+
+//-------------------  3-masala
+ 
+// let a = 11 
+// let b = 9
+
+// for (let i = a; i > b; i--) {
+//     console.log(i);
+// }
+
+
+//-------------------  4-masala
+
+// let sum = 10000
+// let konfet = 10
+
+// for (i = 1; i < konfet; i++) {
+//     console.log(i * sum);
+// }
+
+
+//-------------------  5-masala
+
+// let sum = 10
+// let konfet = 1000
+
+// for (i = 10; i <= konfet; i++) {
+//     console.log(i * sum);
+// }
+
+
+//-------------------  6-masala
+
+// let sum = 1000
+// let total = 20
+
+// for (i = 12; i <= total; i += 2) {
+//     console.log(i * sum);
+// }
+
+
+//-------------------  7-masala
+
+// let a = 1
+// let b = 9
+// let total = 0
+
+// for (let i = 1; i <= b; i++) {
+//     console.log(total += i);
+// }
+
+
+//-------------------  8-masala 
+
+
+// let a = 1
+// let b = 9
+
+// for (let i = 1; i <= b; i++) {
+//     console.log(a *= i);
+// }
+
+
+//-------------------  9-masala 
+
+// let a = 1
+// let b = 9
+
+// for (let i = 1; i <= b; i++) {
+//     console.log(i ** 2);
+// }
+
+
+//-------------------  10-masala 
+
+// Tushunmadim savolni
+
+
+//-------------------  11-masala 
+
+// let a = 9
+
+// for (let i = 0; i <= a; i++) {
+//     if (i % 2) {
+//         console.log(i);
+//     }
+// }
+
+//-------------------  12-masala 
+
+
+// let a = 9
+
+// for (let i = 1; i <= a; i++) {
+//     if (i % 2 == 0) {
+//         console.log(i);
+//     }
+// }
+
+
+//-------------------  13-masala 
+
+// let a = 9
+
+// for (let i = 1; i <= a; i++) {
+//     if (a % i == 0) {
+//         console.log(i);
+//     }
+// }
+
+
+
+
+
+//------------------------------------------------Switch case
+
+//-------------------  1-masala
+
+// const day = prompt('Hafta kunlarini raqamlaridan birini kiriting ')
+
+// switch (day) {
+//     case '1' :
+//         console.log('Dushanba');
+//         break 
+//     case '2' :
+//         console.log('Seshanba');
+//         break 
+//     case '3' :
+//         console.log('Chorshanba');
+//         break 
+//     case '4' :
+//         console.log('Payshanba');
+//         break 
+//     case '5' :
+//         console.log('Juma');
+//         break 
+//     case '6' :
+//         console.log('Shanba');
+//         break 
+//     case '7' :
+//         console.log('Yakshanba');
+//         break    
+//     default :
+//         console.log('Bunday kun yoq');
+// }
+
+
+//-------------------  2-masala
+
+// const baho = prompt('Olgan bahoyizni kiriting')
+
+// switch (baho) {
+//     case '1':
+//         console.log('yomon');
+//         break;
+//     case '2':
+//         console.log('qoniqarsiz');
+//         break;
+//     case '3':
+//         console.log('qoniqarli');
+//         break
+//     case '4':
+//         console.log('yaxshi');
+//         break
+//     case '5':
+//         console.log('alo');
+//         break
+//     default:
+//         console.log('Hazilsiz olgan bahongizni kiriting');
+//         break;
+// }
+
+
+//-------------------  3-masala
+
+// const fasl = prompt('Oylar raqamini kiriting va qaysi oy qaysi falsga tegishliligini bilib oling')
+
+// switch (fasl) {
+//     case '1':
+//         console.log('Yanvar Qish fasli');
+//         break;
+//     case '2':
+//         console.log('Fevral Qish fasli');
+//         break;
+//     case '3':
+//         console.log('Mart Bahor fasli');
+//         break
+//     case '4':
+//         console.log('Aprel Bahor fasli');
+//         break
+//     case '5':
+//         console.log('May Bahor fasli');
+//         break
+//     case '6':
+//         console.log('Iyun Yoz fasli');
+//         break
+//     case '7':
+//         console.log('Iyul Yoz fasli');
+//         break
+//     case '8':
+//         console.log('Avgust Yoz fasli');
+//         break
+//     case '9':
+//         console.log('Sentabr Kuz fasli');
+//         break
+//     case '10':
+//         console.log('Oktabr Kuz fasli');
+//         break
+//     case '11':
+//         console.log('Noyabr Kuz fasli');
+//         break
+//     case '12':
+//         console.log('Dekabr Kuz fasli');
+//         break
+//     default:
+//         console.log('Bunday sondagi oy va fasl yoq');
+//         break;
+// }
+
+//-------------------  4-masala
+
+// const fasl = prompt('Oylardan birini raqami kiriting va qaysi oyda necha kunligini bilib oling')
+
+// switch (fasl) {
+//     case '1':
+//         console.log('Yanvar oyida 31 kun bor');
+//         break;
+//     case '2':
+//         console.log('Fevral oyida 28 yo 29 kun bor');
+//         break;
+//     case '3':
+//         console.log('Mart oyida 31 kun bor');
+//         break
+//     case '4':
+//         console.log('Aprel oyida 30 kun bor');
+//         break
+//     case '5':
+//         console.log('May oyida 31 kun bor');
+//         break
+//     case '6':
+//         console.log('Iyun oyida 30 kun bor');
+//         break
+//     case '7':
+//         console.log('Iyul oyida 31 kun bor');
+//         break
+//     case '8':
+//         console.log('Avgust oyida 31 kun bor');
+//         break
+//     case '9':
+//         console.log('Sentabr oyida 30 kun bor');
+//         break
+//     case '10':
+//         console.log('Oktabr oyida 31 kun bor');
+//         break
+//     case '11':
+//         console.log('Noyabr oyida 30 kun bor');
+//         break
+//     case '12':
+//         console.log('Dekabr oyida 31 kun bor');
+//         break
+//     default:
+//         console.log('Bunday sondagi oy va fasl yoq');
+//         break;
+// }
+
+
+//-------------------  5-masala
+
+// Unchalik tushunmadim.
+
+// let a = 4
+// let b = 5
+// let c = '+'
+
+// switch (a, b, c ) {
+//     case a, c, b:
+//       console.log(a, c, b)
+//       break
+//     default:
+//       console.log('Hato')
+//       break
+//   }
+  
+
+
+//-------------------  6-masala
+
+// const baho = prompt('Olgan bahoyizni kiriting va biz sizga qancha stependiya olishizni aytib beraman')
+
+// switch (baho) {
+//     case '1':
+//         console.log('Hayolizga ham keltirmang');
+//         break;
+//     case '2':
+//         console.log('Harakat qiling oqishga');
+//         break;
+//     case '3':
+//         console.log('Keyingi safar yaxshi oqisangiz olasiz');
+//         break
+//     case '4':
+//         console.log('sizning stependiya 100 ming sum');
+//         break
+//     case '5':
+//         console.log('sizning stependiya 500 ming sum');
+//         break
+//     default:
+//         console.log('Hazilsiz olgan bahongizni kiriting');
+//         break;
+// }
